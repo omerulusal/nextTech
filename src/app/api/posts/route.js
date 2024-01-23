6cd217ed16a50a72d77ac04c008c125b4a3f6bb8
Ömer Ulusal <96357374+omerulusal@users.noreply.github.com>
2024-01-23 21:32:31 +0300

    .

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -5,7 +5,9 @@ export const GET = async (req) => {
     try {
         await connect()
         const posts = await Post.find()
+        //Post koleksiyonundaki tüm belgeleri bulur. posts degişkenine atar 
         return new NextResponse(JSON.stringify(posts), { status: 200 })
+        // posts'u, JSON a dönüştürür ve 200 OK koduyla birlikte yanıtı NextResponse ile geri gönderir.
     } catch (error) {
         
         return new NextResponse("Databese Error", { status: 500 })
